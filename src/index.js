9ee93c773d02f242df1ed1e333cb3bf61f9297b2
skif-cezar <skif.cezar@gmail.com>
2020-03-22 22:14:40 +0300

    decoder solution

--- src/index.js
+++ src/index.js
@@ -39,6 +39,24 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let dot = /10/g;
+    let dash = /11/g;
+    let space = /\*\*\*\*\*\*\*\*\*\*/g;
+    let nothing = /00/g;
+    let arrPhrase = [];
+    let data = expr.toString;
+    let result = "";
+    for (let i = 0; i < expr.length; i += 10) {
+        arrPhrase.push(expr.slice(i, i + 10).replace(nothing, "").replace(dash, "-").replace(dot, ".").replace(space, " "));
+    }
+    arrPhrase.forEach( el => {
+        if (MORSE_TABLE[el] == undefined) {
+        result += " "
+    } else {
+        result += MORSE_TABLE[el];
+    }
+})
+return result;
 }
 
 module.exports = {
